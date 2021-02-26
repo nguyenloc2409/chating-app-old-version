d5739df131c5fe54b12fe68bd68b792c46c5104c
Nguyen Loc <locnguuyenhuu369@gmail.com>
2021-02-26 23:24:41 +0700

    make it better

--- index.js
+++ index.js
@@ -425,10 +425,11 @@ app.post("/getFriends/:id", (req, res) => {
 var mesOfall = [];
 app.post("/getMessage", cors(), (req, res) => {
     //{idMe:'5fdcac08ab75c00024c17bb0', idFriend:'5fdcab92ab75c00024c17baf'}
-    let idMe = '5fdd7fb5700c0400241a50b7' ;
-    let idFriend = "5fdd7f74700c0400241a50b3";
-    //let idMe        = req.body.idMe
-    //let idFriend    = req.body.idFriend
+    // let idMe = '5fdd7fb5700c0400241a50b7' ;
+    // let idFriend = "5fdd7f74700c0400241a50b3";
+    
+    let idMe        = req.body.idMe;
+    let idFriend    = req.body.idFriend;
     Message.find({from:idMe,to:idFriend}, function(err, me){
         Message.find({from:idFriend,to:idMe}, function(err, you){
             you.forEach(function(f){
