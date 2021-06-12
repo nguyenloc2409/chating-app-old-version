ea2072d9545e99cbbd36eca8e334811d23afc7ac
unknown <locnguyenhuu0024@gmail.com>
2021-06-13 01:15:17 +0700

    updated some line

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.set("trust proxy", true);
 app.use(express.static("public"));
 require('dotenv').config();
 
